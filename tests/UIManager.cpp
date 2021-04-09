49d15ede7c0f7e751c679cb5f4e283be7972b8b5
Natnael Eshetu <the.defalt8@gmail.com>
2021-04-09 14:13:03 +0300

    Updated UIManager test

--- tests/UIManager.cpp
+++ tests/UIManager.cpp
@@ -5,15 +5,50 @@ axl::gl::Display g_display;
 
 class MainView : public Test::MainView
 {
+	private:
+		axl::gl::gfx::UIManager m_uimanager;
+		axl::gl::gfx::Font m_default_font;
+		axl::gl::gfx::ui::TextView::Program m_default_text_program;
+		axl::gl::gfx::ui::TextView m_text;
 	public:
 		MainView(const axl::util::WString& _title, const axl::math::Vec2i& _position, const axl::math::Vec2i& _size, const axl::gl::Cursor& _cursor) :
-			Test::MainView(_title, _position, _size, _cursor)
+			Test::MainView(_title, _position, _size, _cursor),
+			m_uimanager(&context),
+			m_default_font(&context),
+			m_default_text_program(&context),
+			m_text(&context)
 		{}
 		~MainView()
 		{
 			this->destroy();
 		}
 	public:
+		bool onCreate(bool recreating)
+		{
+			if(!Test::MainView::onCreate()) return false;
+			m_uimanager.setContext(&context);
+			Assert(m_uimanager.create());
+			m_default_font.setContext(&context);
+			Assert(m_default_font.create());
+			Assert(m_default_font.loadFromFile("../../common/fonts/OpenSans-Light.ttf", axl::math::Vec2i(64,64)));
+			m_default_text_program.setContext(&context);
+			Assert(m_default_text_program.create());
+			m_text.setFont(&m_default_font);
+			m_text.setContext(&context);
+			m_text.setProgram(&m_default_text_program);
+			m_text.setBorderColor(axl::math::Vec4f(1.f,1.f,1.f,1.f));
+			m_text.setBorderSize(axl::math::Vec4f(1.f,1.f,1.f,1.f));
+			m_text.setBackgroundColor(axl::math::Vec4f(1.f,1.f,1.f,.0f));
+			m_text.setTextColor(axl::math::Vec4f(1.f,0.9f,.8f,1.f));
+			m_text.setPadding(axl::math::Vec4f(15.f,15.f,15.f,15.f));
+			Assert(m_text.create());
+			Assert(m_text.setQuality(axl::gl::gfx::ui::TextView::Q_HIGH));
+			Assert(m_text.setText(L"Hello World!"));
+			Assert(m_text.setSize(m_text.getMaxSize()));
+			axl::math::Vec2i text_position = (this->size - m_text.getSize()) / 2;
+			m_text.transform.setPosition(axl::math::Vec3f((float)text_position.x,(float)text_position.y, 0.0f));
+			return true;
+		}
 		bool initialize()
 		{
 			if(!Test::MainView::initialize()) return false;
@@ -32,7 +67,8 @@ class MainView : public Test::MainView
 			using namespace GL;
 			if(!Test::MainView::render()) return false;
 			glClearColor(.1f, .1f, .4f, .0f);
-			glClear(GL_COLOR_BUFFER_BIT);
+			glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+			m_text.render(&main_camera);
 			return true;
 		}
 	
@@ -47,10 +83,10 @@ int main(int argc, char* argv[])
 	puts("----------------------------------------");
 	{
 		// Create View
-		Assert(g_display.isOpen());
+		Assertve(g_display.isOpen(), verbose);
 		axl::math::Vec2i view_position(40, 60), view_size(640, 480);
 		MainView main_view(L"UI Manager", view_position, view_size, axl::gl::Cursor::CUR_CROSS);
-		Assertve(main_view.create(g_display, false, MainView::VF_FIXED), verbose);
+		Assertve(main_view.create(g_display, false, MainView::VF_RESIZABLE), verbose);
 		Assertve(main_view.show(MainView::SM_SHOW), verbose);
 		main_view.initialize();
 		while(!axl::gl::Application::IS_QUITTING)	
