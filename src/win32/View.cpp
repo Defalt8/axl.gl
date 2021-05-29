ba8f08badb8545a80fa121938018f6882d3cfe1d
Natnael Eshetu <the.defalt8@gmail.com>
2021-05-29 15:35:24 +0300

    Added min and max value set based on key input on Slider

--- src/win32/View.cpp
+++ src/win32/View.cpp
@@ -978,8 +978,11 @@ void View::onPointer(int index, int x, int y, bool is_down)
 												if(isInArea(point, client_size, slider->transform.getMatrix(), slider->getContainer()))
 												{
 													float min_value = slider->getMinValue(), max_value = slider->getMaxValue();
-													float slide_value = client_point.x / (client_size.x - (padding.x + padding.z)) * (max_value - min_value);
-													slide_value = axl::math::clamp(slide_value, min_value, max_value);
+													float slide_value = client_point.x / (client_size.x - (padding.x + padding.z));
+													if(axl::gl::input::Keyboard::isKeyDown(axl::gl::input::KeyCode::KEY_CONTROL))
+														slide_value = (slide_value >= 0.5f) ? max_value : min_value;
+													else
+														slide_value = axl::math::clamp((slide_value * (max_value - min_value)), min_value, max_value);
 													slider->onSlide(slide_value);
 													slider->slider_is_sliding = true;
 												}
@@ -1057,8 +1060,11 @@ void View::onPointerMove(int index, int x, int y)
 												axl::math::Vec4f padding(slider->getPadding());
 												axl::math::Vec2f client_point(point.x - position.x - padding.x, point.y - position.y - padding.y);
 												float min_value = slider->getMinValue(), max_value = slider->getMaxValue();
-												float slide_value = client_point.x / (client_size.x - (padding.x + padding.z)) * (max_value - min_value);
-												slide_value = axl::math::clamp(slide_value, min_value, max_value);
+												float slide_value = client_point.x / (client_size.x - (padding.x + padding.z));
+												if(axl::gl::input::Keyboard::isKeyDown(axl::gl::input::KeyCode::KEY_CONTROL))
+													slide_value = (slide_value >= 0.5f) ? max_value : min_value;
+												else
+													slide_value = axl::math::clamp((slide_value * (max_value - min_value)), min_value, max_value);
 												slider->onSlide(slide_value);
 											}
 											else
